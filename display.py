ac7545b6b716ef1f7d51eeb0351030cb4d24ff71
ddoberne <ddoberne@gmail.com>
2023-01-04 09:47:19 -0800

    Update display.py

--- display.py
+++ display.py
@@ -11,6 +11,7 @@ generator = load_generator()
 
 st.title('How are you feeling today?')
 s = st.text_input(label = 'I am feeling...')
+response = None
 if s:
   output = classifier(s)[0]
   if output['score'] > 0.75:
