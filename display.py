20911b1e59f115b9a2203f134b779ed39fae9ca0
ddoberne <ddoberne@gmail.com>
2023-01-04 09:38:03 -0800

    Update display.py

--- display.py
+++ display.py
@@ -1,7 +1,14 @@
 import streamlit as st
 from transformers import pipeline
 
+
+@st.cache
+def load_generator():
+  generator = pipeline('text-generation')
+  return generator
+
 classifier = pipeline("sentiment-analysis")
+generator = load_generator()
 
 st.title('How are you feeling today?')
 s = st.text_input(label = 'I am feeling...')
@@ -15,3 +22,18 @@ if s:
   else:
     response = ':neutral_face:'
   st.title(response)
+  
+if response == ':slightly_frowning_face:':
+  if st.button(label = 'Turn that frown upside down!'):
+    while output['label'] == 'NEGATIVE':
+      benchmark = output['score']
+      generates = generator(s, max_new_tokens = 15, num_return_sequences = 5, return_full_text = False)
+      for generate in generates:
+        generate = generate['generated_text']
+        test_output = classifier(s + generate)[0]
+        if (test_output['label'] == 'POSITIVE') or (test_output['score'] < output['score']):
+          s = s + generate
+          output = test_output
+          break
+      st.write(s)
+      
